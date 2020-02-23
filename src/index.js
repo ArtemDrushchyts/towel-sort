2280c31781f1a95d95c75a8265fe15029e611377
Artem Drushchyts <temafm-3@yandex.ru>
2020-02-23 14:35:37 +0300

    feat: complete task towerSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  
+  let arr = [];
+  for(let i= 0; i < matrix.length; i++) {
+    if(matrix.length === 0 ){
+      return arr;   
+    }
+    else if(i%2  == 1 ){
+      arr = [...arr, ...matrix[i].reverse()]
+    } else {
+      arr = [...arr, ...matrix[i]]
+    }
+  }
+  return arr;
 }
